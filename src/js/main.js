0b927a6ba73812d0146de3a396f69c5c2b790f1d
Kyawt Kyawt San <kyawtkyawtsan@aceplussolutions.com>
2023-09-15 10:41:23 +0630

    added top page simple design

--- src/js/main.js
+++ src/js/main.js
@@ -1,43 +1,44 @@
-import jQuery from 'jquery'
+// import jQuery from 'jquery';
 
-const $ = jQuery;
-window.jQuery = jQuery;
+// const $ = jQuery;
+// window.jQuery = jQuery;
 
-jQuery.extend(jQuery.easing, {
-	def: 'easeOutQuad',
-	easeOutQuart: function (x, t, b, c, d) {
-		return -c * ((t = t / d - 1) * t * t * t - 1) + b;
-	},
-	easeOutExpo: function (e, f, a, h, g) {
-		return f === g ? a + h : h * (-Math.pow(2, -10 * f / g) + 1) + a;
-	},
-	quart: function (x, t, b, c, d) {
-		return -c * ((t = t / d - 1) * t * t * t - 1) + b;
-	}
-});
+// jQuery.extend(jQuery.easing, {
+// 	def: 'easeOutQuad',
+// 	easeOutQuart: function (x, t, b, c, d) {
+// 		return -c * ((t = t / d - 1) * t * t * t - 1) + b;
+// 	},
+// 	easeOutExpo: function (e, f, a, h, g) {
+// 		return f === g ? a + h : h * (-Math.pow(2, -10 * f / g) + 1) + a;
+// 	},
+// 	quart: function (x, t, b, c, d) {
+// 		return -c * ((t = t / d - 1) * t * t * t - 1) + b;
+// 	}
+// });
 
-// win8.1 ie11 mousewheel scroll bug
-if (navigator.userAgent.match(/Trident\/7\./)) {
-	$('body').on('mousewheel', function () {
-		event.preventDefault();
-		var wheelDelta = event.wheelDelta;
-		var currentScrollPosition = window.pageYOffset;
-		window.scrollTo(0, currentScrollPosition - wheelDelta);
-	});
-}
+// $(window).on({
+//   load: function () {
+//     // footerScr();
+//   },
+//   resize: function () {
+//     // footerScr();
+//   },
+//   scroll: function () {
+//     // footerScr();
+//   }
+// });
 
-$(window).on({
-  load: function () {
-    // footerScr();
-  },
-  resize: function () {
-    // footerScr();
-  },
-  scroll: function () {
-    // footerScr();
-  }
-});
+// $('a[href^="#"]').click(function (e) {
+// 	e.preventDefault();
+// });
 
-$('a[href^="#"]').click(function (e) {
-	e.preventDefault();
-});
+// $('.p-slider__area--mainvisual').slick({
+//   fade: true,
+//   autoplay: true,
+//   pauseOnHover: false,
+//   autoplaySpeed: 3000,
+//   speed: 800,
+//   arrows: true,
+//   dots: true,
+//   appendDots: $('.p-mainvisual__dots')
+// });
